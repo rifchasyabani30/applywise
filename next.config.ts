4b2af9795bd1ca8cf9fd86d891e9aefeeb2ae723
ranggagilang <gilangyanuarirangga@gmail.com>
2026-08-31 21:30:48 +0700

    Perbarui Halaman Utama

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/marketing/landingpage",
+        permanent: true, // atau false jika halaman utama bersifat dinamis
+      },
+    ];
+  },
 };
 
 export default nextConfig;
